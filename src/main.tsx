d4e6bb745ddc640d1694f59150a17277090047ab
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-10-12 03:21:03 -0300

    chore: Add react-router-dom for routing and error handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,29 @@
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, Spinner } from "@chakra-ui/react";
+import {
+	createBrowserRouter,
+	RouterProvider,
+	useRouteError,
+} from "react-router-dom";
+
+import Error from "./pages/Error/Error";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>Home</div>,
+		errorElement: <Error />,
+	},
+	{
+		path: "/login",
+		element: <div>login</div>,
+	},
+	{
+		path: "/dashboard",
+		element: <div>dashboard</div>,
+	},
+]);
 
 if (!document.getElementById("app"))
 	document.body.appendChild(document.createElement("div")).id = "app";
@@ -8,7 +31,9 @@ if (!document.getElementById("app"))
 createRoot(document.getElementById("app")!).render(
 	<StrictMode>
 		<ChakraProvider>
-			<Suspense fallback={<Spinner />}></Suspense>
+			<Suspense fallback={<Spinner />}>
+				<RouterProvider router={router} />
+			</Suspense>
 		</ChakraProvider>
 	</StrictMode>
 );
