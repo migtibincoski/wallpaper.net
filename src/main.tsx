0a9c079f76685a855aca194171b82941a06201ad
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2024-10-12 03:20:28 -0300

    feat: Add Chakra UI and lazy loading for images

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { ChakraProvider, Spinner } from "@chakra-ui/react";
 
 if (!document.getElementById("app"))
 	document.body.appendChild(document.createElement("div")).id = "app";
 
-createRoot(document.getElementById("app")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("app")!).render(
+	<StrictMode>
+		<ChakraProvider>
+			<Suspense fallback={<Spinner />}></Suspense>
+		</ChakraProvider>
+	</StrictMode>
+);
